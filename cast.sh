b9aa5c34e9fcbd6a64a4b7482be179c11e3de180
simo94pasqui <simo94.pasqualini@gmail.com>
2024-09-10 22:10:33 +0200

    Update systemctl cast service

--- cast.sh
+++ cast.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 cmdCatt="/home/pi/.local/bin/catt"
+deviceCucina="192.168.178.104"
 
-while true
-do
-  if ! $cmdCatt -d "Cucina" status | grep 'PLAYING'; then 
-      $cmdCatt -d "Cucina" volume 0
-      $cmdCatt -d "Cucina" cast_site http://192.168.178.232:10014/lovelace/default_view
-  fi
-  sleep 60
-done
+if ! $cmdCatt -d $deviceCucina info | grep 'display_name: DashCast'; then
+    $cmdCatt -d $deviceCucina volume 0
+    $cmdCatt -d $deviceCucina cast_site http://192.168.178.232:10014/lovelace/default_view
+fi
